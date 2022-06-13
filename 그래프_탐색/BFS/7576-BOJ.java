4cdba45688e5b2f4d73590f3c5aadfdfeb66c14e
Donghwa Shin <50476562+ehdghk154@users.noreply.github.com>
2022-06-13 20:04:43 +0900

    Update 7576-BOJ.java

--- 그래프_탐색/BFS/7576-BOJ.java
+++ 그래프_탐색/BFS/7576-BOJ.java
@@ -1,10 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/*
+ * 7576 토마토
+ */
 class pos {
   int x;
   int y;
