993142083d0723454cacdca96f734dd199e13577
Donghwa Shin <50476562+ehdghk154@users.noreply.github.com>
2022-06-27 19:06:33 +0900

    Update 11653-BOJ.java

--- 수학/정수론/11653-BOJ.java
+++ 수학/정수론/11653-BOJ.java
@@ -4,7 +4,6 @@
 import java.io.*;
 
 public class Main {
-  static int sum = 0;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
