bf615d760f58a634e71a37187ad09a153e4dae0f
Donghwa Shin <50476562+ehdghk154@users.noreply.github.com>
2022-06-29 20:25:15 +0900

    Create 1912-BOJ.java

---
+++ 동적_계획법/1912-BOJ.java
@@ -0,0 +1,29 @@
+/*
+ * 1912 연속합
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] arr = new int[N];
+    int[] dp = new int[N];
+    
+    for(int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    
+    dp[0] = arr[0];
+    int max = arr[0];
+    
+    for(int i = 1; i < N; i++) {
+      dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+      max = Math.max(dp[i], max);
+    }
+    
+    System.out.println(max);
+  }
+}
