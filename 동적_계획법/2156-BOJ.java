cff1bfe2118f9ec3d987e896b771aba6cb991890
Donghwa Shin <50476562+ehdghk154@users.noreply.github.com>
2022-07-01 19:50:00 +0900

    Update 2156-BOJ.java

--- 동적_계획법/2156-BOJ.java
+++ 동적_계획법/2156-BOJ.java
@@ -7,7 +7,6 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    StringBuilder sb = new StringBuilder();
     int[] dp = new int[n+1];
     int[] arr = new int[n+1];
     for(int i = 1 ; i <= n; i++) {
