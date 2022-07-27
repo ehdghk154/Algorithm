79fa3bfc5817e5909d4671e60cbcbe7a5e7504ad
Donghwa Shin <50476562+ehdghk154@users.noreply.github.com>
2022-07-27 22:28:36 +0900

    Create 15651-BOJ.java

---
+++ 백트래킹/15651-BOJ.java
@@ -0,0 +1,71 @@
+/*
+ * 15651 N과 M (3)
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static int[] arr;
+  private static int N, M;
+  static StringBuilder sb;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    sb = new StringBuilder();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    
+    arr = new int[M];
+    
+    bt(0);
+    
+    System.out.println(sb);
+  }
+  private static void bt(int depth) {
+    if(depth == M) {
+      for(int num : arr)
+        sb.append(num).append(" ");
+      sb.append("\n");
+      
+      return;
+    }
+    
+    for(int i=0; i<N; i++) {
+      arr[depth] = i+1;
+      bt(depth+1);
+    }
+  }
+}
+
+/*
+N = 3, M = 3 일 때,
+depth / num
+0 1
+1 1
+2 1
+3 print 1 1 1 -> return;
+
+2 2
+3 print 1 1 2 -> return;
+
+2 3
+3 print 1 1 3 -> return;
+
+2 end
+
+1 2
+2 1
+3 print 1 2 1 -> return;
+...
+
+0 3
+1 1
+2 1
+3 print 3 1 1 -> return;
+...
+
+2 3
+3 print 3 3 3 -> return;
+
+end
+*/
