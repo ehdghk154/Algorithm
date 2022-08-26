fad9fbde4743cf871ddf9b368e27856157d9dd3c
Donghwa Shin <50476562+ehdghk154@users.noreply.github.com>
2022-08-26 17:59:44 +0900

    Create 2444-BOJ.java

---
+++ 구현/2444-BOJ.java
@@ -0,0 +1,27 @@
+/*
+ * 2444 별 찍기 - 7
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    
+    for(int i = 1; i <= N; i++) {
+      sb.append(" ".repeat(N-i));
+      sb.append("*".repeat(2*i-1));
+      sb.append("\n");
+    }
+    
+    for(int i = N-1; i > 0; i--) {
+      sb.append(" ".repeat(N-i));
+      sb.append("*".repeat(2*i-1));
+      sb.append("\n");
+    }
+    
+    System.out.print(sb);
+  }
+}
