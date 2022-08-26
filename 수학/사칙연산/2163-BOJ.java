bfa91dbd1964c56046dc564ac62094c876828981
Donghwa Shin <50476562+ehdghk154@users.noreply.github.com>
2022-08-26 19:06:43 +0900

    Create 2163-BOJ.java

---
+++ 수학/사칙연산/2163-BOJ.java
@@ -0,0 +1,16 @@
+/*
+ * 2163 초콜릿 자르기
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    
+    System.out.print(N*M-1);
+  }
+}
