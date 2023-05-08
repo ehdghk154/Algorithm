8ef671780ef0a3ee97fdc1ac20e6130014cd703e
ehdghk154 <ehdghk154@naver.com>
2023-05-08 20:06:02 +0900

    add 크레인_인형뽑기_게임.java(Lv. 1)

---
+++ Programmers/java/Lv. 1/크레인_인형뽑기_게임.java
@@ -0,0 +1,31 @@
+/**
+ * 크레인 인형뽑기 게임 (Lv. 1)
+ * https://school.programmers.co.kr/learn/courses/30/lessons/64061
+ */
+
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < moves.length; i++) {
+            for(int j = 0; j < board.length; j++) {
+                if(board[j][moves[i]-1] != 0) {
+                    if(!stack.isEmpty() && stack.peek() == board[j][moves[i]-1]) {
+                        stack.pop();
+                        answer += 2;
+                    }else {
+                        stack.add(board[j][moves[i]-1]);
+                    }
+                    board[j][moves[i]-1] = 0;
+                    break;
+                }
+                if(!stack.isEmpty())
+                	System.out.println(stack.toString());
+            }
+        }
+        
+        return answer;
+    }
+}
