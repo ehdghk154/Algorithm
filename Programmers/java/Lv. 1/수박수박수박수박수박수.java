6a1ca918e7d27cabc13d5514782986fa93d16d26
ehdghk154 <ehdghk154@naver.com>
2023-07-24 19:53:58 +0900

    add 소수_찾기.java(Lv. 1)

--- Programmers/java/Lv. 1/수박수박수박수박수박수.java
+++ Programmers/java/Lv. 1/수박수박수박수박수박수.java
@@ -4,11 +4,11 @@
  */
 
 class Solution {
+	// n이 최대 10000이므로 먼저 "수박수박수..."를 1만자까지 생성
+    String word = "수박".repeat(5000);
     public String solution(int n) {
         String answer = "";
-        // n이 최대 10000이므로 먼저 "수박수박수..."를 1만자까지 생성
-        String word = "수박".repeat(5000);
-
+        
         // n까지의 문자를 substring으로 자르기
         answer = word.substring(0, n);
 
