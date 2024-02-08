5a8489af940f93b2c55de645227dc325dbb31982
ehdghk154 <ehdghk154@naver.com>
2024-02-08 23:30:57 +0900

    fix: 로직 수정 및 주석 추가 작성

--- Programmers/java/Lv. 2/124 나라의 숫자.java
+++ Programmers/java/Lv. 2/124 나라의 숫자.java
@@ -22,7 +22,7 @@ class Solution {
 			위와 같이 표시된 3의 배수들 외에는 모두 3진법과 동일
 			3의 배수 관련 로직을 작성하면
 			n값을 3으로 나눈 나머지가 0이면 4로 처리 후
-			n 3으로 나눈 몫에 -1을 해서 계산하면 해당 값이 나옴
+			몫에 -1을 하고 계산하면 해당 값이 나옴
          */
         while(n > 0) {
         	int t = n % 3;
@@ -30,10 +30,7 @@ class Solution {
         		sb.append(4);
         	else
         		sb.append(t);
-        	n /= 3;
-        	if(t == 0) {
-        		n -= 1;
-        	}
+        	n = (n-1) / 3;
         }
         
         answer = sb.reverse().toString();
