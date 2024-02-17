152910c7427e103e6315d5510005b0588ad6b265
ehdghk154 <ehdghk154@naver.com>
2024-02-17 18:01:51 +0900

    add: 카테고리 별 상품 개수 구하기.sql(Lv. 2)

---
+++ Programmers/MySQL/Lv. 2/카테고리 별 상품 개수 구하기.sql
@@ -0,0 +1,15 @@
+/**
+ * 카테고리 별 상품 개수 구하기(Lv. 2)
+ * https://school.programmers.co.kr/learn/courses/30/lessons/131529
+ */
+
+
+SELECT 
+    LEFT(PRODUCT_CODE, 2) AS CATEGORY,
+    COUNT(PRODUCT_CODE) AS PRODUCTS
+FROM 
+    PRODUCT
+GROUP BY --제품코드 앞 2자리가 같은 것 끼리 묶음
+    LEFT(PRODUCT_CODE, 2)
+ORDER BY --제품코드 앞 2자리로 내림차순 정렬
+    LEFT(PRODUCT_CODE, 2)
