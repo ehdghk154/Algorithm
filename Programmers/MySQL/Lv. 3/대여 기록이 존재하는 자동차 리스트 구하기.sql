5fc6a96d67d6e9ebbafc53d27ed2ee1b547ea619
ehdghk154 <ehdghk154@naver.com>
2024-02-28 22:01:16 +0900

    대여 기록이 존재하는 자동차 리스트 구하기.sql(Lv. 3)

--- Programmers/MySQL/Lv. 3/대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ Programmers/MySQL/Lv. 3/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -3,7 +3,7 @@
  * https://school.programmers.co.kr/learn/courses/30/lessons/157341
  */
 
-SELECT -- DISTINCT c1, c2, ... => 중복 제거 컬럼이 여러개면 모든 컬럼의 값이 동일한 내용 중복을 제거
+SELECT -- DISTINCT c1, c2, ... => 중복 제거. 컬럼이 여러개면 모든 컬럼의 값이 동일한 내용 중복을 제거
 	DISTINCT CAR_ID 
 FROM 
 	CAR_RENTAL_COMPANY_CAR JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY USING(CAR_ID)
